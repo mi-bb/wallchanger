ac476520445dd2ab609ffdb723fd72b9292d275e
Michal Babik <michalb1981@o2.pl>
2019-11-09 00:19:41 +0100

    Update to v1.1.1

--- settstr.h
+++ settstr.h
@@ -1,26 +1,26 @@
 /**
- * @file  settstr.h
- * @copyright Copyright (C) 2019 Michał Bąbik
- *
- * This file is part of Wall Changer.
- *
- * Wall Changer is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Wall Changer is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Wall Changer.  If not, see <https://www.gnu.org/licenses/>.
- *
- * @brief  Wallpaper settings WallSett structure
- * 
- * @author Michał Bąbik <michalb1981@o2.pl>
- */
+* @file  settstr.h
+* @copyright Copyright (C) 2019 Michał Bąbik
+*
+* This file is part of Wall Changer.
+*
+* Wall Changer is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Wall Changer is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Wall Changer.  If not, see <https://www.gnu.org/licenses/>.
+*
+* @brief  Wallpaper settings WallSett structure and functions
+* 
+* @author Michał Bąbik <michalb1981@o2.pl>
+*/
 #ifndef SETTSTR_H
 #define SETTSTR_H
 #include <glib.h>
