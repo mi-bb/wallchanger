cb1e6bfa41c5333a7d26dde873f6f8e70cf9dd41
Michal Babik <michalb1981@o2.pl>
2020-01-15 07:25:59 +0100

    Update to 1.3.7

--- src/setting.c
+++ src/setting.c
@@ -24,7 +24,6 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-//#include "strfun.h"
 #include "hashfun.h"
 #include "setting.h"
 /*----------------------------------------------------------------------------*/
