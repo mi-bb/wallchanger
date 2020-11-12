126955f0db36e03d3d9a25a8b9469c149eafec54
Michal Babik <michalb1981@o2.pl>
2020-11-12 17:32:58 +0100

    Code updates

--- src/webwidget_c.c
+++ src/webwidget_c.c
@@ -168,11 +168,6 @@ ww_get_api_key_data (Setting  *st_settings,
     for (int i = 0; i < 4; ++i) {
         *fs_data->s_str[i] = strdup ("");
     }
-//    fs_data->s_str1 = strdup ("");
-//    fs_data->s_str2 = strdup ("");
-//    fs_data->s_str3 = strdup ("");
-//    fs_data->s_str4 = strdup ("");
-
     for (int i = 0; i < 4; ++i) {
         if (*fs_key_names->s_str[i][0] != '\0') {
             st_sett = settings_find (st_settings, *fs_key_names->s_str[i]);
@@ -180,26 +175,6 @@ ww_get_api_key_data (Setting  *st_settings,
             str_append (fs_data->s_str[i], s_api);
         }
     }
-//    if (fs_key_names->s_str1[0] != '\0') {
-//        st_sett = settings_find (st_settings, fs_key_names->s_str1);
-//        s_api   = st_sett == NULL ? "" : setting_get_string (st_sett);
-//        str_append (&fs_data->s_str1, s_api);
-//    }
-//    if (fs_key_names->s_str2[0] != '\0') {
-//        st_sett = settings_find (st_settings, fs_key_names->s_str2);
-//        s_api   = st_sett == NULL ? "" : setting_get_string (st_sett);
-//        str_append (&fs_data->s_str2, s_api);
-//    }
-//    if (fs_key_names->s_str3[0] != '\0') {
-//        st_sett = settings_find (st_settings, fs_key_names->s_str3);
-//        s_api   = st_sett == NULL ? "" : setting_get_string (st_sett);
-//        str_append (&fs_data->s_str3, s_api);
-//    }
-//    if (fs_key_names->s_str4[0] != '\0') {
-//        st_sett = settings_find (st_settings, fs_key_names->s_str4);
-//        s_api   = st_sett == NULL ? "" : setting_get_string (st_sett);
-//        str_append (&fs_data->s_str4, s_api);
-//    }
     fourstrings_free (fs_key_names);
 
     return fs_data;
@@ -223,11 +198,6 @@ ww_update_api_key_data (const char  *s_cfg_file,
 
 #ifdef HAVE_FLICKCURL
     if (i_site == WEB_WIDGET_FLICKR) {
-//        for (int i = 1; i < 4; ++i) {
-//            st_setts = settings_append (st_setts,
-//                setting_new_string (*fs_key_names->s_str[i],
-//                                    *fs_key_data->s_str[i]));
-//        }
         st_setts = settings_append (st_setts,
             setting_new_string (fs_key_names->s_str2, fs_key_data->s_str2));
         st_setts = settings_append (st_setts,
